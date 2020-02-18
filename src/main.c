24caa1eed26065486cf780f5550ceb87d68b2aa5
swornim00 <swornimshrestha@Swornims-MacBook-Pro.local>
2020-02-18 15:18:34 +0545

    Print String added

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include<util/delay.h>
 
 #define F_CPU 16000000UL
-#define LCD_RS PB4
-#define LCD_EN PB3
+#define LCD_RS PB0
+#define LCD_EN PB1
 #define DATA_PORT PORTD
 #define COMMAND_PORT PORTB
 
@@ -12,7 +12,7 @@ void test(uint8_t data){ // command deko
     DDRD = 0XFF;
 
     PORTD = data ; // giving data
-    COMMAND_PORT |= ~(1 << LCD_RS); // RS pin low
+    COMMAND_PORT &= ~(1 << LCD_RS); // RS pin low
 
     // Giving a pulse
     PORTB |= (1 << LCD_EN); // Enable pin high
@@ -22,25 +22,52 @@ void test(uint8_t data){ // command deko
 }
 
 void print(char data){ // print gareko
-     DDRB = 0xFF;
+    DDRB = 0xFF;
     DDRD = 0XFF;
 
     PORTD = data ; // giving data
     COMMAND_PORT |= (1 << LCD_RS); // RS pin high
 
     // Giving a pulse
+    PORTB &= ~(1 << LCD_EN); // Enable pin low
+    _delay_us(1);
     PORTB |= (1 << LCD_EN); // Enable pin high
     _delay_us(1);
     PORTB &= ~(1 << LCD_EN); // Enable pin low
-    _delay_ms(3);
+    _delay_ms(0.3);
+
 }
 
+void lcd_puts(char *string) {
+  for (char *it = string; *it; it++) {
+    print(*it);
+  }
+}
+
+void clear_screen(){
+    test(0x01);
+}
 
 int main(){
-    _delay_ms(20);		/* LCD Power ON delay always >15ms */
+    _delay_ms(40);		/* LCD Power ON delay always >15ms */
 	test(0x38);	/* Initialization of 16X2 LCD in 8bit mode */
+    test(0x0E);
+    clear_screen();
+    _delay_ms(2);
     test(0x01);
-	test(0x0C);	/* Display ON Cursor OFF */
-    print('A');
+    while(1){
+        clear_screen();
+	lcd_puts("Puja");
+    _delay_ms(1000);
+    clear_screen();
+	lcd_puts("Shivani");
+    _delay_ms(1000);
+
+    clear_screen();
+	lcd_puts("Swornim");
+    _delay_ms(1000);
+    }
+
 }
+
 
